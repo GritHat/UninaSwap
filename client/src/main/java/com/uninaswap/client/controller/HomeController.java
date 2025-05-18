f7904b01a79b74d2e16a9cfedb9785b4028d467c
Federico Dellini <federicodellini@gmail.com>
2025-05-19 01:53:28 +0200

    problem on dimension homepage

--- client/src/main/java/com/uninaswap/client/controller/HomeController.java
+++ client/src/main/java/com/uninaswap/client/controller/HomeController.java
@@ -1,27 +1,58 @@
 package com.uninaswap.client.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
+import javafx.scene.image.Image;
 
 public class HomeController {
 
+    @FXML private HBox articoliPreferitiBox;
+    @FXML private HBox utentiPreferitiBox;
+    @FXML private HBox astePreferiteBox;
+
     @FXML
     public void initialize() {
-        System.out.println("Inizializzazione HomeController");
+        // Esempio: popola articoli preferiti con dati fittizi
+        articoliPreferitiBox.getChildren().clear();
+        for (int i = 1; i <= 3; i++) {
+            VBox card = new VBox();
+            ImageView img = new ImageView(new Image(getClass().getResourceAsStream("/new/UI_img/spermatozoi.png")));
+            img.setFitWidth(100);
+            img.setFitHeight(100);
+            Text titolo = new Text("Articolo " + i);
+            Text prezzo = new Text((10 * i) + "€");
+            card.getChildren().addAll(img, titolo, prezzo);
+            articoliPreferitiBox.getChildren().add(card);
+        }
+
+        // Esempio: popola utenti preferiti con dati fittizi
+        utentiPreferitiBox.getChildren().clear();
+        for (int i = 1; i <= 2; i++) {
+            VBox userBox = new VBox();
+            ImageView img = new ImageView(new Image(getClass().getResourceAsStream("/new/UI_img/spermatozoi.png")));
+            img.setFitWidth(100);
+            img.setFitHeight(100);
+            Text nome = new Text("Utente " + i);
+            userBox.getChildren().addAll(img, nome);
+            utentiPreferitiBox.getChildren().add(userBox);
+        }
+
+        // Esempio: popola aste preferite con dati fittizi
+        astePreferiteBox.getChildren().clear();
+        for (int i = 1; i <= 2; i++) {
+            VBox astaBox = new VBox();
+            Text titolo = new Text("Asta " + i);
+            Text offerta = new Text("Offerta: " + (i * 50) + "€");
+            astaBox.getChildren().addAll(titolo, offerta);
+            astePreferiteBox.getChildren().add(astaBox);
+        }
     }
-    
+
     @FXML
     public void apri_profilo() {
-        System.out.println("Apertura profilo");
-        // Implementare la navigazione alla pagina del profilo
+        // Implementa la logica di apertura profilo
     }
-    
-    // Altri metodi di gestione eventi della home page
 }

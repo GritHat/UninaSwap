34a942ca2b1b26cfd32558b825a7cb397092d199
Federico Dellini <federicodellini@gmail.com>
2025-05-20 23:58:50 +0200

    creazione itemCardController

--- client/src/main/java/com/uninaswap/client/controller/MainController.java
+++ client/src/main/java/com/uninaswap/client/controller/MainController.java
@@ -9,8 +9,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.shape.Circle;
+import javafx.scene.text.Text;
 
 import com.uninaswap.client.service.NavigationService;
 import com.uninaswap.client.constants.EventTypes;
@@ -55,6 +58,10 @@ public class MainController implements Refreshable {
         this.imageService = ImageService.getInstance();
     }
 
+    @FXML private HBox articoliPreferitiBox;
+    @FXML private HBox utentiPreferitiBox;
+    @FXML private HBox astePreferiteBox;
+    
     @FXML
     public void initialize() {
         checkAuthentication();
@@ -78,6 +85,42 @@ public class MainController implements Refreshable {
         eventBus.subscribe(EventTypes.LOCALE_CHANGED, _ -> {
             Platform.runLater(this::refreshAllViews);
         });
+
+        //load cards
+        // Esempio: popola articoli preferiti con dati fittizi
+        articoliPreferitiBox.getChildren().clear();
+        for (int i = 1; i <= 3; i++) {
+            VBox card = new VBox();
+            ImageView img = new ImageView(new Image(getClass().getResourceAsStream("/new/UI_img/spermatozoi.png")));
+            img.setFitWidth(100);
+            img.setFitHeight(100);
+            Text titolo = new Text("Articolo " + i);
+            Text prezzo = new Text((10 * i) + "€");
+            card.getChildren().addAll(img, titolo, prezzo);
+            articoliPreferitiBox.getChildren().add(card);
+        }
+
+        // Esempio: popola utenti preferiti con dati fittizi
+        utentiPreferitiBox.getChildren().clear();
+        for (int i = 1; i <= 2; i++) {
+            VBox userBox = new VBox();
+            ImageView img = new ImageView(new Image(getClass().getResourceAsStream("/new/UI_img/spermatozoi.png")));
+            img.setFitWidth(100);
+            img.setFitHeight(100);
+            Text nome = new Text("Utente " + i);
+            userBox.getChildren().addAll(img, nome);
+            utentiPreferitiBox.getChildren().add(userBox);
+        }
+
+        // Esempio: popola aste preferite con dati fittizi
+        astePreferiteBox.getChildren().clear();
+        for (int i = 1; i <= 2; i++) {
+            VBox astaBox = new VBox();
+            Text titolo = new Text("Asta " + i);
+            Text offerta = new Text("Offerta: " + (i * 50) + "€");
+            astaBox.getChildren().addAll(titolo, offerta);
+            astePreferiteBox.getChildren().add(astaBox);
+        }
     }
 
     @FXML
@@ -179,13 +222,6 @@ public class MainController implements Refreshable {
     public void openAste(ActionEvent event) {
 
     }
-    //Cards menager
-
-
-
-
-
-
 
     /**
      * Verify that a user is logged in, redirect to login if not
